c614ab82e0728fd7dd2b7125c1d3482b10448f87
Schendel <r.van_schendel@lumc.nl>
2026-01-23 13:52:14 +0100

    add welcome tab

--- MUSIC_R/functions/tabs.R
+++ MUSIC_R/functions/tabs.R
@@ -1,6 +1,13 @@
 generateTabs <- function(data_input) {
   if (data_input == "1") {
     tabsetPanel(id = "tabs",
+                tabPanel(
+                  "Welcome",
+                  h1("Welcome"),
+                  p("This application provides multiple interactive visualizations for exploring gene-level and barcode-level data."),
+                  p("Use the tabs above to navigate between tables and plots, including waterfall, XY, and volcano plots."),
+                  p("Hover over plots or select data points where available to view additional details.")
+                ),
                 tabPanel("Genes & Barcodes", h1("genes and barcodes"), withSpinner(DT::dataTableOutput("gene_barcode"))),
                 tabPanel("Waterfall", uiOutput("UIWaterfall")),
                 tabPanel("XY", p("XY plot is meant to quickly ascertain if a Gene deviates..."), uiOutput("UIXYplot"), uiOutput("hover_xy")),
